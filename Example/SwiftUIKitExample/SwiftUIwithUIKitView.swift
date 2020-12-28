5316aa7784bd4be62ca715538745c92d5f1c2d6f
Antoine van der Lee <ajvanderlee@gmail.com>
2020-12-28 16:13:21 +0100

    Add an extra UILabel example

--- Example/SwiftUIKitExample/SwiftUIwithUIKitView.swift
+++ Example/SwiftUIKitExample/SwiftUIwithUIKitView.swift
@@ -12,7 +12,7 @@ struct SwiftUIwithUIKitView: View {
     var body: some View {
         NavigationView {
             UIKitView() // <- This is a `UIKit` view.
-                .swiftUIView(layout: .intrinsic)
+                .swiftUIView(layout: .intrinsic) // <- This is a SwiftUI `View`.
                 .set(\.title, to: "Hello, UIKit!")
                 .set(\.backgroundColor, to: UIColor(named: "swiftlee_orange"))
                 .fixedSize()
@@ -24,5 +24,15 @@ struct SwiftUIwithUIKitView: View {
 struct SwiftUIwithUIKitView_Previews: PreviewProvider {
     static var previews: some View {
         SwiftUIwithUIKitView()
+    }
+}
+
+// MARK: - UILabel Preview Example
+struct UILabelExample_Preview: PreviewProvider {
+    static var previews: some View {
+        UILabel() // <- This is a `UIKit` view.
+            .swiftUIView(layout: .intrinsic) // <- This is a SwiftUI `View`.
+            .set(\.text, to: "Hello, UIKit!")
+            .preview(displayName: "UILabel Preview Example")
     }
 }
