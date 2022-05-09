8f0239c2cab94a3f6c6a76e204061e5ac4436d04
Antoine van der Lee <ajvanderlee@gmail.com>
2022-05-09 08:50:17 +0200

    Remove unused test

--- Tests/SwiftUIKitViewTests/SwiftUIKitViewTests.swift
+++ Tests/SwiftUIKitViewTests/SwiftUIKitViewTests.swift
@@ -2,14 +2,5 @@ import XCTest
 @testable import SwiftUIKitView
 
 final class SwiftUIKitViewTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftUIKitView().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    
 }
