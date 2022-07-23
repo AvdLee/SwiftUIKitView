ebb2e1ab252b23389e1703d5b96e7e7e33493458
Antoine van der Lee <ajvanderlee@gmail.com>
2022-07-23 18:38:33 +0200

    Iteration 3

--- Example/SwiftUIKitExample/SwiftUIwithUIKitView.swift
+++ Example/SwiftUIKitExample/SwiftUIwithUIKitView.swift
@@ -18,10 +18,15 @@ struct SwiftUIwithUIKitView: View {
 //                    $0.text = "Hello no \(self.integer) from UIKit"
 //                }
 //                .fixedSize()
+                UIViewContainer(UILabel(), layout: .intrinsic)
+                    .set(\.text, to: "Hello, UIKit \(integer)!")
+                    .set(\.backgroundColor, to: UIColor(named: "swiftlee_orange"))
+//                    .fixedSize()
                 UIViewContainer(UIKitView(), layout: .intrinsic)
                     .set(\.title, to: "Hello, UIKit \(integer)!")
                     .set(\.backgroundColor, to: UIColor(named: "swiftlee_orange"))
                     .fixedSize()
+//                    .fixedSize()
                     .navigationTitle("Use UIKit in SwiftUI")
                 Button("RANDOMIZED: \(integer)") {
                     integer = Int.random(in: 0..<300)
--- Sources/SwiftUIKitView/UIViewContainer.swift
+++ Sources/SwiftUIKitView/UIViewContainer.swift
@@ -9,6 +9,86 @@ import Foundation
 import UIKit
 import SwiftUI
 
+public class UIViewContainingCoordinator<Child: UIView> {
+    private(set) var view: Child?
+    private var viewCreator: () -> Child
+
+    var widthConstraint: NSLayoutConstraint?
+    var heightConstraint: NSLayoutConstraint?
+    private let layout: UIViewContainer<Child>.Layout
+
+    init(_ viewCreator: @escaping () -> Child, layout: UIViewContainer<Child>.Layout) {
+        self.viewCreator = viewCreator
+        self.layout = layout
+    }
+
+    func createView() {
+        guard view == nil else { return }
+        view = viewCreator()
+        view?.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+    func updateSize(for view: Child) {
+        switch layout {
+        case .intrinsic:
+            let size = self.size(for: view)
+            update(view: view, width: size.width, height: size.height)
+        case .fixed(let size):
+            update(view: view, width: size.width, height: size.height)
+        case .fixedWidth(let width):
+            update(view: view, width: width, height: nil)
+        }
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+        view.setNeedsUpdateConstraints()
+        view.updateConstraints()
+    }
+
+    private func update(view: Child, width: CGFloat?, height: CGFloat?) {
+        if let width = width {
+            if let widthConstraint = widthConstraint {
+                widthConstraint.constant = width
+            } else {
+                widthConstraint = view.widthAnchor.constraint(equalToConstant: width)
+                widthConstraint?.isActive = true
+            }
+        }
+        if let height = height {
+            if let heightConstraint = heightConstraint {
+                heightConstraint.constant = height
+            } else {
+                heightConstraint = view.heightAnchor.constraint(equalToConstant: height)
+                heightConstraint?.isActive = true
+            }
+        }
+    }
+
+    /// - Returns: The `CGSize` to apply to the view.
+    func size(for view: Child) -> CGSize {
+        switch layout {
+        case .fixedWidth(let width):
+            // Set the frame of the cell, so that the layout can be updated.
+            var newFrame = view.frame
+            newFrame.size = CGSize(width: width, height: UIView.layoutFittingExpandedSize.height)
+            view.frame = newFrame
+
+            // Make sure the contents of the cell have the correct layout.
+            view.setNeedsLayout()
+            view.layoutIfNeeded()
+
+            // Get the size of the cell
+            let computedSize = view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+
+            // Apple: "Only consider the height for cells, because the contentView isn't anchored correctly sometimes." We use ceil to make sure we get rounded numbers and no half pixels.
+            return CGSize(width: width, height: ceil(computedSize.height))
+        case .fixed(let size):
+            return size
+        case .intrinsic:
+            return view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        }
+    }
+}
+
 /// A container for UIKit `UIView` elements. Conforms to the `UIViewRepresentable` protocol to allow conversion into SwiftUI `View`s.
 @available(iOS 13.0, *)
 public struct UIViewContainer<Child: UIView> { //}: Identifiable {
@@ -27,72 +107,15 @@ public struct UIViewContainer<Child: UIView> { //}: Identifiable {
         /// A fixed width and height is used.
         case fixed(size: CGSize)
     }
-    
-    var view: Child! {
-        get { coordinator.view }
-    }
-    private let viewCreator: () -> Child
-    private let layout: Layout
-    private let coordinator: Coordinator<Child>
 
-    /// - Returns: The `CGSize` to apply to the view.
-    private var size: CGSize {
-        switch layout {
-        case .fixedWidth(let width):
-            // Set the frame of the cell, so that the layout can be updated.
-            var newFrame = view.frame
-            newFrame.size = CGSize(width: width, height: UIView.layoutFittingExpandedSize.height)
-            view.frame = newFrame
-            
-            // Make sure the contents of the cell have the correct layout.
-            view.setNeedsLayout()
-            view.layoutIfNeeded()
-            
-            // Get the size of the cell
-            let computedSize = view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
-            
-            // Apple: "Only consider the height for cells, because the contentView isn't anchored correctly sometimes." We use ceil to make sure we get rounded numbers and no half pixels.
-            return CGSize(width: width, height: ceil(computedSize.height))
-        case .fixed(let size):
-            return size
-        case .intrinsic:
-            return view.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
-        }
-    }
+    @State public var coordinator: UIViewContainingCoordinator<Child>
     
     /// Initializes a `UIViewContainer`
     /// - Parameters:
     ///   - view: `UIView` being previewed
     ///   - layout: The layout to apply on the `UIView`. Defaults to `intrinsic`.
-    public init(_ view: @escaping @autoclosure () -> Child, layout: Layout = .intrinsic) {
-        self.viewCreator = view
-        self.layout = layout
-        self.coordinator = Coordinator(self.viewCreator)
-    }
-    
-    /// Applies the correct size to the SwiftUI `View` container.
-    /// - Returns: A `View` with the correct size applied.
-//    public func fixedSize() -> some View {
-//        let size = self.size
-//        return frame(width: size.width, height: size.height, alignment: .topLeading)
-//    }
-    
-    /// Creates a preview of the `UIViewContainer` with the right size applied.
-    /// - Returns: A preview of the container.
-    public func preview(displayName: String? = nil) -> some View {
-        return fixedSize()
-            .previewLayout(.sizeThatFits)
-            .previewDisplayName(displayName)
-    }
-
-    public class Coordinator<Child> {
-        var view: Child!
-        var viewCreator: () -> Child
-        var modifiers: [(Child) -> Void] = []
-
-        init(_ viewCreator: @escaping () -> Child) {
-            self.viewCreator = viewCreator
-        }
+    public init(_ viewCreator: @escaping @autoclosure () -> Child, layout: Layout = .intrinsic) {
+        self.coordinator = UIViewContainingCoordinator(viewCreator, layout: layout)
     }
 }
 
@@ -100,44 +123,88 @@ public struct UIViewContainer<Child: UIView> { //}: Identifiable {
 
 @available(iOS 13, *)
 extension UIViewContainer: UIViewRepresentable {
-    public func makeCoordinator() -> Coordinator<Child> {
+    public func makeCoordinator() -> UIViewContainingCoordinator<Child> {
         // Create an instance of Coordinator
         coordinator
     }
 
     public func makeUIView(context: Context) -> Child {
-        self.coordinator.view = viewCreator()
-        coordinator.modifiers.forEach { modifier in
-            modifier(self.coordinator.view)
-        }
-        switch layout {
-        case .intrinsic:
-            return view
-        case .fixed(let size):
-            self.view.widthAnchor.constraint(equalToConstant: size.width).isActive = true
-            self.view.heightAnchor.constraint(equalToConstant: size.height).isActive = true
-        case .fixedWidth(let width):
-            self.view.widthAnchor.constraint(equalToConstant: width).isActive = true
-        }
-        return view
+        context.coordinator.createView()
+        return context.coordinator.view!
     }
     
     public func updateUIView(_ view: Child, context: Context) {
-        coordinator.modifiers.forEach { modifier in
-            modifier(view)
-        }
+        update(view)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+
+    public func update(_ uiView: Child) {
+        coordinator.updateSize(for: uiView)
     }
 }
 
-@available(iOS 13.0, *)
-extension UIViewContainer: KeyPathReferenceWritable {
-    public typealias T = Child
-    
-    public func set<Value>(_ keyPath: ReferenceWritableKeyPath<Child, Value>, to value: Value) -> Self {
-        coordinator.modifiers.append({ view in
-            view[keyPath: keyPath] = value
-        })
-        print("Modifiers is now \(coordinator.modifiers)")
-        return self
+public protocol UIViewContaining: UIViewRepresentable {
+    associatedtype Child: UIView
+    var coordinator: UIViewContainingCoordinator<Child> { get }
+    func set<Value>(_ keyPath: ReferenceWritableKeyPath<Child, Value>, to value: Value) -> ModifiedUIViewContainer<Self, Child, Value>
+    func update(_ uiView: Child)
+}
+
+extension UIViewContaining {
+    public func set<Value>(_ keyPath: ReferenceWritableKeyPath<Child, Value>, to value: Value) -> ModifiedUIViewContainer<Self, Child, Value> {
+        ModifiedUIViewContainer(child: self, keyPath: keyPath, value: value)
+    }
+
+    /// Applies the correct size to the SwiftUI `View` container.
+    /// - Returns: A `View` with the correct size applied.
+//    public func fixedSizing() -> some View {
+//        if let view = coordinator.view {
+//            update(coordinator.view!)
+//            let size = coordinator.size(for: view)
+//            return self.frame(width: size.width, height: size.height, alignment: .topLeading)
+//        } else {
+//            return self
+//        }
+//    }
+
+}
+
+public extension View {
+    /// Creates a preview of the `UIViewContainer` with the right size applied.
+    /// - Returns: A preview of the container.
+    func preview(displayName: String? = nil) -> some View {
+        return fixedSize()
+            .previewLayout(.sizeThatFits)
+            .previewDisplayName(displayName)
+    }
+}
+
+extension UIViewContainer: UIViewContaining { }
+public struct ModifiedUIViewContainer<ChildContainer: UIViewContaining, Child, Value>: UIViewContaining where ChildContainer.Child == Child {
+    var child: ChildContainer
+    public var coordinator: UIViewContainingCoordinator<Child> {
+        child.coordinator
+    }
+    @State var keyPath: ReferenceWritableKeyPath<Child, Value>
+    @State var value: Value
+
+    public func makeCoordinator() -> UIViewContainingCoordinator<Child> {
+        coordinator
+    }
+
+    public func makeUIView(context: Context) -> Child {
+        coordinator.createView()
+        return coordinator.view!
+    }
+
+    public func updateUIView(_ uiView: Child, context: Context) {
+        update(uiView)
+    }
+
+    public func update(_ uiView: Child) {
+        uiView[keyPath: keyPath] = value
+        child.update(uiView)
+        coordinator.updateSize(for: uiView)
     }
 }
